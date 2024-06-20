1254bfd20fe01dabe43f1cbacec951f760c96ac9
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-06-20 18:03:35 -0500

    feat: :sparkles: Filtrar personas por el nombre o descripción
    Funcionalidad para buscar nombres o descripciones de las personas filtrandolas con el mockapi en tiempo real

--- js/main.js
+++ js/main.js
@@ -6,26 +6,24 @@ let main_section = document.querySelector(".main_section");
 
 addEventListener("DOMContentLoaded", async () => {
     let info = await getAllInfoPeople();
+    console.log(info);
 
-    main_section.innerHTML = await profiles(info);
+    main_section.innerHTML = await profiles(info); // Cargue personas por default
     
-    const names = () => {
-        let array = [];
-        for (let i = 0; i < info.length; i++) {
-            array.push(info[i].name_full);
-        }
-        return array;
-    };
 
     search.addEventListener("input", async e => {
-        // let namePerson = names()
+        
+        const searchTerm = e.target.value.toLowerCase().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Obtener el valor del input y convertirlo a minusculas, sin espacios y sin tildes
 
-        // namePerson.filter(letra => {
-        //     if (e.target.value === namePerson) {
-        //         console.log(namePerson[i])
-        //     }
-        // })
+        const filteredProfiles = info.filter(person => {  // Comparar si el nombre o descripción de la persona incluye el termino de busqueda
+            return person.name_full.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(searchTerm) || person.description.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(searchTerm); 
+        });
 
-
+        if (filteredProfiles.length === 0) { // Si no se encuentran personas
+            main_section.innerHTML = /*html*/ `<h2>No se encontraron resultados</h2>
+                <style>.main_section{display: flex; justify-content: center; align-items: center; margin-top: 50px; color: rgb(13, 160, 106)}</style>`;
+        } else {
+            main_section.innerHTML = await profiles(filteredProfiles);
+        }
     });
 })
